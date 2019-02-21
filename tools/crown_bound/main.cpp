f9905cabca9f01d862d38b80e079281119f9feca
Vasily Alferov <ya-ikmik2012@yandex.ru>
2019-02-21 17:46:18 +0300

    Fix crown bound

--- tools/crown_bound/main.cpp
+++ tools/crown_bound/main.cpp
@@ -1,4 +1,5 @@
 #include <graph/graph.h>
+#include <kernels/common.h>
 #include <kernels/crown/crown.h>
 #include <kernels/crown/crown_2k.h>
 
@@ -9,8 +10,7 @@ int main() {
     PaceVC::Graph h = g;
 
     PaceVC::Kernels::CrownKernel(g).reduce();
-    PaceVC::Kernels::Crown2kKernel(h).reduce();
-    std::cerr << h.size() << " " << h.solution().size() << std::endl;
+    PaceVC::Kernels::Exhaustive<PaceVC::Kernels::Crown2kKernel>(h).reduce();
     std::cout << std::max((h.size() + 1) / 2 + h.solution().size(), (g.size() + 2) / 3 + g.solution().size()) << std::endl;
     return 0;
 }
