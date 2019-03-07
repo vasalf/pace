2757224331cc257b55739383a04165be085496b6
Vasily Alferov <ya-ikmik2012@yandex.ru>
2019-03-07 16:06:07 +0300

    Fix components a bit

--- tools/comps/main.cpp
+++ tools/comps/main.cpp
@@ -1,10 +1,12 @@
 #include <graph/graph.h>
 #include <graph/util.h>
+#include <kernels/trivial.h>
 
 #include <iostream>
 
 int main() {
     PaceVC::Graph g = PaceVC::readGraph(std::cin);
+    PaceVC::Kernels::Trivial(g).reduce();
     std::cout << PaceVC::connectedComponents(g).size() << std::endl;
     return 0;
 }
